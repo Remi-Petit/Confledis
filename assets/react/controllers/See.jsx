cebf53060495788b05bd82c8e532d2bbd03acb49
master <remi.petit93370@gmail.com>
2023-07-24 01:43:06 +0200

    Filtre React + CSS

--- assets/react/controllers/See.jsx
+++ assets/react/controllers/See.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
 
 export default function See() {
 
-    const [datas, setDatas] = useState([]);
+    const [objet, setObjet] = useState([]);
+    const [dataObjet, setDataObjet] = useState([]);
 
     //http://127.0.0.1:8000/api/produits      https://jsonplaceholder.typicode.com/posts
 
     useEffect(() => {
+        const dataObjet =
         fetch('http://127.0.0.1:8000/api/produits')
             .then((response) => response.json())
-            .then((json) => setDatas(json));
-    }, [])
+            .then((json) => setObjet(json['hydra:member']));
+    },  [])
+    
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/produits')
+            .then((response) => response.json())
+            .then((json) => setDataObjet(json['hydra:member']));
+    },  [])
 
-    console.log(datas);
-    console.log(datas[1]);
+    //console.log(objet);
     
     return (
+        
         <>
-            <div>
-                {datas.map((post) => {
-                    return <div> {post.context} </div>;
-                })}
-            </div>
+            <tr>
+                <td><SearchBar placeholder={"filtre par ID"} filterField={(objet) => objet.id.toString()} list={dataObjet} setList={setObjet}></SearchBar></td>
+                <td><SearchBar placeholder={"filtre par nom"} filterField={(objet) => objet.nom.toString()} list={dataObjet} setList={setObjet}></SearchBar></td>
+                <td><SearchBar placeholder={"filtre par prix"} filterField={(objet) => objet.prix.toString()} list={dataObjet} setList={setObjet}></SearchBar></td>
+                <td><SearchBar placeholder={"filtre par quantite"} filterField={(objet) => objet.quantite.toString()} list={dataObjet} setList={setObjet}></SearchBar></td>
+            </tr>
+
+            {objet.map((objet, index) => {
+                return <tr key={index}>
+                <td>{ objet.id }</td>
+                <td>{ objet.nom }</td>
+                <td>{ objet.prix }</td>
+                <td>{ objet.quantite }</td>
+                <td>
+                <a href={objet.id}>show</a>
+                <a href={objet.id +'/edit'}>edit</a>
+                </td>
+            </tr>;
+            })}
         </>
     )
 }
